fa7ddee3f4949b9b10b4656fb16a2a52a59c4969
Deborah Erhahi <99527771+Erhahi@users.noreply.github.com>
2025-07-19 15:14:05 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Data paths
+RAW_DATA_PATH = "../data/jumia_jitters_dataset.csv"
+CLEANED_DATA_PATH = "../cleaned/cleaned_orders.csv"
+SELLER_SUMMARY_PATH = "../cleaned/seller_summary.csv"
+
+# Model paths
+MODEL_PATH = "../models/return_model.pkl"
+SCALER_PATH = "../models/return_scaler.pkl"
+
+# Output
+PREDICTIONS_OUTPUT_PATH = "../outputs/return_predictions.csv"
